034d758bb231982a9186f9f415e0c935ae3adc04
Lunik <lunik@tiwabbit.fr>
2021-01-23 18:59:17 +0100

    Fix commit count
    Signed-off-by: Lunik <lunik@tiwabbit.fr>

--- api/gitea/gitea.py
+++ api/gitea/gitea.py
@@ -5,7 +5,10 @@ def get_repo_commit_count(gitea, repo):
 
   response = gitea.requests.get(gitea.url + "/api/v1" + page_endpoint + "?limit=1", headers=gitea.headers, params={})
 
-  return int(response.headers.get('X-Total'))
+  if 'X-Total' in response.headers:
+    return int(response.headers.get('X-Total'))
+  else:
+    return 0
 
 def gitea_export(app):
   metrics = dict()
